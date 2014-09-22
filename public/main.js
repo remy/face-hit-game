012ee421088e4c7d5dd04e7cdd2f319c3c080a1d
Remy Sharp <remy@remysharp.com>
2014-09-22 18:39:47 +0300

    fix intro

--- public/main.js
+++ public/main.js
@@ -49,3 +49,4 @@ $('#upload').on('change', function () {
   readfile(this.files[0]);
 });
 
+alert('Instructions:\n1. Upload your face.\n2. You get a get colour.\n3. Score points by hitting the faces with same colour ring.');
